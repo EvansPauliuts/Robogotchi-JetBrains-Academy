3bd683ebb09f18033ff0f52632c66faf67ec4bcb
Evans Pauliuts <pauliutsevans@gmail.com>
2022-01-26 21:38:28 +0300

    Work on project. Stage 2/4: Rock-Paper-Scissors

--- README.md
+++ README.md
@@ -1,80 +1,69 @@
 # Robogotchi-JetBrains-Academy
 This project jetbrains academy https://hyperskill.org/projects/135
 
-## Work on project. Stage 1/4: The game of numbers
+## Work on project. Stage 2/4: Rock-Paper-Scissors
 ### Objectives
 
 In this stage, your program should:
 
-1. Take the user's input, check its validity, and ask again for a valid input if needed.
-2. If the user provides an invalid option, the program has to display an appropriate message.
-All three invalid options — negative numbers, text strings, and numbers over 1,000,000 — have to be handled differently.
-3. Output the result of the game.
-4. Keep taking the input as long as the user wants to play.
-5. Collect the game statistics and print it when the user decides to exit the game.
+1. Prompt the user to pick a game with a message ```Which game would you like to play?```.
+2. If they choose "Numbers", proceed with the game as in the previous stage. If they choose rock-paper-scissors, 
+implement steps 3-6. If the user input is anything else than "Numbers" or "Rock-paper-scissors", 
+print ```Please choose a valid option: Numbers or Rock-paper-scissors?```.
+3. Take the user input, check its validity, and ask for a valid input if needed.
+4. Output the robot's move: ```The robot chose [paper/rock/scissors]```.
+5. Output the result using an appropriate message: ```You won!```, ```The robot won!```, or ```It's a draw!```.
+6. Keep taking input as long as the user wants to play.
+7. Keep the game statistics and print it when the user decides to exit the game.
 
 #### Examples
 The greater-than symbol followed by a space (>) represents the user input.
 
 ##### example 1
 ```shell
-What is your number?
-> -54
+Which game would you like to play?
+> Rock-paper-scissors
 
-The number can't be negative!
+What is your move?
+> Paper
+The robot chose paper
+It's a draw!
 
-What is your number?
-> 45
-
-The robot entered the number 810376.
-The goal number is 63419.
+What is your move?
+> rock
+The robot chose scissors
 You won!
 
-What is your number?
+What is your move?
+> gun
+No such option! Try again!
+
+What is your move?
+> scissors
+The robot chose paper
+You won!
+
+What is your move?
 > exit game
 
-You won: 1,
+You won: 2,
 The robot won: 0,
-Draws: 0.
+Draws: 1
 ```
 
 ##### example 2
 ```shell
-What is your number?
-> 5000001
+Which game would you like to play?
+> Dominoes
 
-Invalid input! The number can't be bigger than 1000000.
+Please choose a valid option: Numbers or Rock-paper-scissors?
 
-What is your number?
-> 654
-
-The robot entered the number 235945.
-The goal number is 875951.
-The robot won!
-
-What is your number?
-> 123123
-
-The robot entered the number 810808.
-The goal number is 875951.
-The robot won!
-
-What is your number?
-> 432443
-
-The robot entered the number 555335.
-The goal number is 875951.
-The robot won!
-
-What is your number?
-> pineapple
-
-A string is not a valid input!
+> Numbers
 
 What is your number?
 > exit game
 
 You won: 0,
-The robot won: 3,
+The robot won: 0,
 Draws: 0.
 ```
--- robogotchi.py
+++ robogotchi.py
@@ -1,5 +1,6 @@
 # Write your code here
-from random import randint
+from random import randint, choice
+from enum import Enum
 
 
 class NegativeError(Exception):
@@ -10,6 +11,12 @@ class NegativeError(Exception):
 class RangeError(Exception):
     def __str__(self):
         return '\nInvalid input! The number can\'t be bigger than 1000000.\n'
+
+
+class RandomRPS(Enum):
+    rock = 'rock'
+    paper = 'paper'
+    scissors = 'scissors'
 
 
 class RoboGoNumber:
@@ -23,23 +30,29 @@ class RoboGoNumber:
             'draws': 0
         }
 
-    def process_task(self, input_str):
-        try:
-            input_num = int(input_str)
+    def process_task(self):
+        while True:
+            user_val = input('\nWhat is your number?\n')
 
-            if input_num < RoboGoNumber.MIN_NUM:
-                raise NegativeError
-            elif input_num > RoboGoNumber.MAX_NUM:
-                raise RangeError
+            if user_val == 'exit game':
+                break
+            else:
+                try:
+                    input_num = int(user_val)
 
-        except ValueError:
-            print('\nA string is not a valid input\n')
-        except NegativeError as e:
-            print(e)
-        except RangeError as e:
-            print(e)
-        else:
-            self.process_compute(input_num)
+                    if input_num < RoboGoNumber.MIN_NUM:
+                        raise NegativeError
+                    elif input_num > RoboGoNumber.MAX_NUM:
+                        raise RangeError
+
+                except ValueError:
+                    print('\nA string is not a valid input\n')
+                except NegativeError as e:
+                    print(e)
+                except RangeError as e:
+                    print(e)
+                else:
+                    self.process_compute(input_num)
 
     def process_compute(self, b):
         randint_number = randint(RoboGoNumber.MIN_NUM, RoboGoNumber.MAX_NUM)
@@ -60,19 +73,61 @@ class RoboGoNumber:
             self.total_count['robot_won'] += 1
             print('The robot won!')
 
-    def run(self):
-        while True:
-            user_input = input('What is your number?\n')
-
-            if user_input == 'exit game':
-                self.get_prints_info()
-                break
-            else:
-                self.process_task(user_input)
-
     def get_prints_info(self):
         print(f'\nYou won: {self.total_count["won"]},\n'
               f'The robot won: {self.total_count["robot_won"]},\nDraws: {self.total_count["draws"]}.')
+        print()
+
+    @staticmethod
+    def winner_check(a, b):
+        winning_dict = {
+            'rock': 'scissors',
+            'scissors': 'paper',
+            'paper': 'rock'
+        }
+
+        if a in winning_dict.keys():
+            return a if b == winning_dict[a] else b
+
+    def rock_paper_scissors(self):
+        while True:
+            user_val = input('\nWhat is your move?\n')
+            robot_random = choice(list(RandomRPS))
+
+            if user_val == robot_random.value:
+                self.total_count['draws'] += 1
+                print(f'The robot chose {robot_random.value}')
+                print(f'It\'s a draw!')
+            elif user_val == self.winner_check(user_val, robot_random.value):
+                self.total_count['won'] += 1
+                print(f'The robot chose {robot_random.value}')
+                print('You won!')
+            elif user_val == 'exit game':
+                break
+            elif user_val not in ['paper', 'scissors', 'rock']:
+                print('No such option! Try again!')
+            else:
+                self.total_count['robot_won'] += 1
+                print(f'The robot chose {robot_random.value}')
+                print('The robot won!')
+
+    def run(self):
+        print('Which game would you like to play?')
+        while True:
+            user_input = input()
+
+            if user_input in ('Numbers', 'numbers'):
+                self.process_task()
+                break
+            elif user_input in ('Rock-paper-scissors', 'rock-paper-scissors'):
+                self.rock_paper_scissors()
+                break
+            else:
+                print('\nPlease choose a valid option: Numbers or Rock-paper-scissors?')
+                continue
+
+        if self.get_prints_info() is not None:
+            print(self.get_prints_info())
 
 
 def main():
